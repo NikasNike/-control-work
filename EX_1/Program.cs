a69571572970a82d3242e51ae0ee0bdca4c5cee4
Nikas Nike <Nikasnike759@gmail.com>
2022-01-15 18:11:34 +0300

    написал программу

--- EX_1/Program.cs
+++ EX_1/Program.cs
@@ -3,5 +3,25 @@ Random rnd = new Random ();
 int [] Mass = new int [10];
 int lenght  = Mass.Length;
 
+for (int i = 0; i < lenght; i++)
+    {
+        Mass[i] = (new Random()).Next(1, 20);
+        Console.WriteLine(Mass[i]);
 
+    }
+    Console.WriteLine();
 
+for(int i=0,j=0; i<Mass.Length && j < Mass2.Length; i++)
+    {
+        if (Mass[i]%2 == 0)
+            {
+                Mass2[j] = Mass[i];
+                j++;
+            }
+            
+    }
+for(int j=0; j < Mass2.Length; j++)
+{
+    if(Mass2[j]>0)
+    System.Console.WriteLine(Mass2[j]);
+}
